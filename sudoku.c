0238449fac60f57d2d51e21d83d4fe490fc2f9e5
RamiroMagnater1 <>
2022-05-15 18:28:05 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -69,7 +69,7 @@ List* get_adj_nodes(Node* n){
           Node * new = copy(n);
           new->sudo[i][j] = cont;
           pushFront(list,new);
-          cont++;
+          cont--;
         }
         return list; 
       }
