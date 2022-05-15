f9d8867b89542170a15f44d8c842de7ceda15979
RamiroMagnater1 <>
2022-05-15 18:51:40 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -50,7 +50,7 @@ int is_valid(Node* n){
   for(f = 0 ; f < 9 ; f++){
     int fila[10] = {0};
     for(c = 0 ; c < 9 ; c++){
-      if(fila[n->sudo[f][c]] == 1) return 0;
+      if(fila[n->sudo[f][c]] != 0) return 0;
       if(fila[n->sudo[f][c]] == 0 && fila[n->sudo[f][c]] != 0){
         fila[n->sudo[f][c]] = 1;
       }
@@ -60,7 +60,7 @@ int is_valid(Node* n){
   for(f = 0 ; f < 9 ; f++){
     int columna[10] = {0};
     for(c = 0 ; c < 9 ; c++){
-      if(columna[n->sudo[c][f]] == 1) return 0;
+      if(columna[n->sudo[c][f]] != 0) return 0;
       if(columna[n->sudo[c][f]] == 0 && columna[n->sudo[c][f]] != 0){
         columna[n->sudo[c][f] = 1;
       }
