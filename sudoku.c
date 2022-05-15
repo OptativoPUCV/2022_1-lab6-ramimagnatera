dbfcf02865c6ef764c2b23d69919ef5c361deaba
RamiroMagnater1 <>
2022-05-15 19:32:55 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -46,6 +46,7 @@ void print_node(Node* n){
 int is_valid(Node* n){
   int f = 0;
   int c = 0;
+  
   //filas
   for(f = 0 ; f < 9 ; f++){
     int fila[10] = {0};
@@ -56,29 +57,33 @@ int is_valid(Node* n){
       }
     }
   }
+  
   //columna
   for(f = 0 ; f < 9 ; f++){
     int columna[10] = {0};
     for(c = 0 ; c < 9 ; c++){
       if(columna[n->sudo[c][f]] != 0) return 0;
       if(columna[n->sudo[c][f]] == 0 && n->sudo[c][f] != 0){
+        
         columna[n->sudo[c][f] = 1;
       }
     }
   }
-  /*
+  
+  
   for (f = 0; f < 9; f++){
+    int aux = f,c;
+    printf("aux");
     int matriz[10] = {0};
     for (c = 0; c < 9 ; c++){
-      if (matriz[10][n->sudo[f][c]] != 0)
-        return 0;
-      if(validMatrix[n->sudo[f][c]] == 0 && n->sudo[f][c] != 0)
+      if (matriz[10][n->sudo[f][c]] != 0) return 0;
+      if(validMatrix[n->sudo[f][c]] == 0 && n->sudo[f][c] != 0){
       matriz[n->sudo[f][c]]=1;
+      }
+    }
   }    
-  */
   return 1;
 }
-
 
 List* get_adj_nodes(Node* n){
   List* list=createList();
